5a520a2bd1b37f70566888604016fa8848aec9dc
Sean Dwyer <spdwyer06@gmail.com>
2021-06-10 13:49:07 -0400

    Initial commit

---
+++ UserRolesDemo_Web/Controllers/RoleController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRolesDemo_Web.Models;
+
+namespace UserRolesDemo_Web.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            ViewBag.Roles = _db.Roles.ToList();
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Role model)
+        {
+            var role = new IdentityRole
+            {
+                //Id = Guid.NewGuid().ToString(),
+                Name = model.Name
+            };
+
+            _db.Roles.Add(role);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ UserRolesDemo_Web/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserRolesDemo_Web.Models
+{
+    public class Role
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
